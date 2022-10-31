fef83b5565ec36220a3a4ce3bf2586d2cd018ab2
Jogchum1 <jogchum1@hotmail.nl>
2022-10-31 15:35:30 +0100

    Added first steps notebook and scriptable objects

---
+++ Assets/Scripts/ScriptableObjects/Clue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Clue", menuName = "Clue")]
+
+public class Clue : ScriptableObject
+{
+    public new string clue;
+    public string description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
