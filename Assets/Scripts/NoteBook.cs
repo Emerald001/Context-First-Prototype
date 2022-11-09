08cb58cd4dc966a148f8ce704c9d80434fbadd6c
Jogchum1 <jogchum1@hotmail.nl>
2022-11-09 16:11:04 +0100

    Fixed collectables notebook

--- Assets/Scripts/NoteBook.cs
+++ Assets/Scripts/NoteBook.cs
@@ -7,12 +7,16 @@ public class NoteBook : MonoBehaviour
     public GameObject prefab;
     public Transform NoteBookCollectibleParent;
     public Transform ToggleParent;
-    private bool toggleBool;
+    public bool BookActive;
     public CollectiblePagesList NoteBookList = new CollectiblePagesList();
     public List<Page> CurrentPageList = new List<Page>();
+    public List<GameObject> prefabList = new List<GameObject>();
 
     public int currentPage;
     private bool BookCreated;
+    private GameObject TempPrefab;
+    public int CollectiblesFound;
+    private int index2;
 
     //public List<ScriptableObject> CollectiblesList = new List<ScriptableObject>();
     // Start is called before the first frame update
@@ -20,47 +24,110 @@ public class NoteBook : MonoBehaviour
     {
         ToggleParent.gameObject.SetActive(false);
         currentPage = 0;
-        BookCreated = false;
+        BookActive = false;
+        prefabList = new List<GameObject>();
+
+        NoteBookList.CollectiblesList.Add(new Page());
+        NoteBookList.CollectiblesList[0].PageList = new List<ScriptableObject>();
+
+        CollectiblesFound = 0;
+        index2 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && BookActive == false)
         {
-            toggleBool = !toggleBool;
-            ToggleParent.gameObject.SetActive(toggleBool);
-
-            if(BookCreated == false)
-            {
-                CurrentPageList.Add(NoteBookList.CollectiblesList[0]);
-                BookCreated = true;
-                
-            }
+            ToggleParent.gameObject.SetActive(true);
+            UpdatePages();
+            BookActive = true;
         }
 
+        else if (Input.GetKeyDown(KeyCode.Escape) && BookActive == true)
+        {
+            ToggleParent.gameObject.SetActive(false);
+            UpdatePages();
+            BookActive = false;
+        }
     }
 
     public void AddCollectibleToNoteBook(ScriptableObject objectToAdd)
     {
+        CurrentPageList.Clear();
+        CurrentPageList.Add(NoteBookList.CollectiblesList[0]);
+        prefab.GetComponent<CollectibleDisplay>().collectible = (Collectible)objectToAdd;
+        Instantiate(prefab, NoteBookCollectibleParent);
+    }
 
-        for (int i = 0; i < CurrentPageList.Count; i++)
+    public void UpdatePages()
+    {
+
+        foreach (GameObject prefabItem in prefabList)
         {
-            prefab.GetComponent<CollectibleDisplay>().collectible = (Collectible)objectToAdd;
-            Instantiate(prefab, NoteBookCollectibleParent);
+            Destroy(prefabItem);
+        }
+
+        prefabList.RemoveAll(GameObject => GameObject == null);
+        prefabList.Clear();
+        CurrentPageList.Clear();
+
+        if(prefabList.Count == 0)
+        {
+            CurrentPageList.Add(NoteBookList.CollectiblesList[currentPage]);
+
+            foreach (Collectible item in CurrentPageList[0].PageList)
+            {
+                prefab.GetComponent<CollectibleDisplay>().collectible = (Collectible)item;
+                TempPrefab = Instantiate(prefab, NoteBookCollectibleParent);
+                prefabList.Add(TempPrefab);
+            }
+        }
+    }
+
+    public void ManageCollectibles(ScriptableObject scriptableObject)
+    {
+        CollectiblesFound++;
+
+        if (CollectiblesFound <= 6)
+        {
+            int index = NoteBookList.CollectiblesList.Count - 1;
+            NoteBookList.CollectiblesList[index].PageList.Add(scriptableObject);
+
+            if (CollectiblesFound >= 6)
+            {
+                ResetList();
+            }
+        }
+    }
+
+    public void ResetList()
+    {
+        CollectiblesFound = 0;
+        NoteBookList.CollectiblesList.Add(new Page());
+        index2++;
+        NoteBookList.CollectiblesList[index2].PageList = new List<ScriptableObject>();
+    }
+
+
+    public void PageRight()
+    {
+        Debug.Log(NoteBookList.CollectiblesList.Count);
+        if(currentPage < NoteBookList.CollectiblesList.Count -1)
+        {   
+            currentPage++;
+            UpdatePages();
         }
 
     }
 
-    public void PageRight()
-    {
-        currentPage++;
-        CurrentPageList.Clear();
-    }
-
     public void PageLeft()
     {
-        currentPage--;
-        CurrentPageList.Clear();
+        if(currentPage > 0)
+        {
+            currentPage--;
+            UpdatePages();
+
+        }
     }
 }
--- Assets/Scripts/TakeCollectible.cs
+++ Assets/Scripts/TakeCollectible.cs
@@ -5,45 +5,13 @@ using UnityEngine;
 public class TakeCollectible : MonoBehaviour, IInteractable
 {
     public ScriptableObject scriptableObject;
-
     public NoteBook noteBook;
-    public int CollectiblesFound;
-    public int index2;
-    public void Start()
-    {
-        //noteBook = GetComponent<NoteBook>();
-        CollectiblesFound = 0;
-        index2 = 0;
-        noteBook.NoteBookList.CollectiblesList.Add(new Page());
-        noteBook.NoteBookList.CollectiblesList[index2].PageList = new List<ScriptableObject>();
-    }
+
+    
+    
     public void Interact()
     {
-        CollectiblesFound++;
-        if(CollectiblesFound <= 6)
-        {
-            int index = noteBook.NoteBookList.CollectiblesList.Count - 1;
-            noteBook.NoteBookList.CollectiblesList[index].PageList.Add(scriptableObject);
-            noteBook.AddCollectibleToNoteBook(scriptableObject);
-
-            Debug.Log("INTERACT");
-            Debug.Log("Index 1 = " + index);
-            Debug.Log("Index 2 = " + index2);
-            
-            if(CollectiblesFound >= 6)
-            {
-                ResetList();
-            }
-
-        }
-        
+        noteBook.ManageCollectibles(scriptableObject);
     }
-
-    public void ResetList()
-    {
-        CollectiblesFound = 0;
-        noteBook.NoteBookList.CollectiblesList.Add(new Page());
-        index2++;
-        noteBook.NoteBookList.CollectiblesList[index2].PageList = new List<ScriptableObject>();
-    }
+    
 }
