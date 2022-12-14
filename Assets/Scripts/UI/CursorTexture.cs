d4034170907cfd13fdc41658166646c90a885eec
Andries <61500390+Emerald001@users.noreply.github.com>
2022-12-14 10:16:50 +0100

    Fixed Camera Clipping and prepared the people for dialog

--- Assets/Scripts/UI/CursorTexture.cs
+++ Assets/Scripts/UI/CursorTexture.cs
@@ -5,22 +5,17 @@ using UnityEngine;
 public class CursorTexture : MonoBehaviour
 {
     public Texture2D point;
-    Texture2D grab;
+    public Texture2D grab;
     public Texture2D open;
 
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
         Cursor.SetCursor(point, hotSpot, cursorMode);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
